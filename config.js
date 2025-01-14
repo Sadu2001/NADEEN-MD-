16fa220cadd277907a72038b843d5caa98ea9a87
Sadu2001 <sandushadewmina010302@gmail.com>
2025-01-14 16:09:14 +0530

    Update config.js

--- config.js
+++ config.js
@@ -5,7 +5,7 @@ function convertToBool(text, fault = 'true') {
     return text === fault ? true : false;
 }
 module.exports = {
-SESSION_ID: process.env.SESSION_ID || "", //put your session id
+SESSION_ID: process.env.SESSION_ID || "5s5XEIYD#rsFJfpQ2B7V1cTXX718MiRrF53wXE4mkouR8fIIUVZ4", //put your session id
 MONGODB: process.env.MONGODB || "mongodb+srv://sam:sam@cluster0.u1smxsv.mongodb.net/?retryWrites=true&w=majority",
 MODE: process.env.MODE || "private", //private or public
 AUTO_VOICE:"true" //true or false
